b5cfb51548926eacea994a2b719bf2823116f14d
Prabh710 <prabhjeetsingh710@outlook.com>
2024-02-13 18:44:32 -0500

    first

---
+++ service/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.lang.String;
+
+/**
+ * The type Main.
+ */
+public class Main {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
